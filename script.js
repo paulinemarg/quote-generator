5c2cbe86c678e6a6f93d6325920a1cb460989308
paulinemarg <paulinemargossian@gmail.com>
2023-05-23 15:11:41 +0200

    add: loader

--- script.js
+++ script.js
@@ -3,10 +3,24 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes =[];
 
+// Show loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide loading
+function complete() {
+  loader.hidden = true;
+  quoteContainer.hidden = false;
+}
+
 function newQuote() {
+  loading();
   // Pick a random quote
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]; 
   if (!quote.author) {
@@ -20,10 +34,12 @@ function newQuote() {
     quoteText.classList.remove('long-quote');
   }
   quoteText.textContent = quote.text;
+  complete();
 }
 
 // Get Quotes from API
 async function getQuotes() {
+  loading();
   const apiUrl ='https://type.fit/api/quotes';
   try {
     const response = await fetch(apiUrl);
